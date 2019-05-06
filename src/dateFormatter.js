8c1276a176fa0079b98bb1a496e940aafdf8486b
aliasgar makda <makdaa1@gmail.com>
2019-05-06 19:28:54 +0100

    working solution

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,15 @@
+const dateFns = require("date-fns")
+
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  const dateToFormatTime = dateFns.startOfDay(dateToFormatTimeMillis)
+  const systemDate = dateFns.startOfDay(systemDateTimeMillis)
+
+  if (dateFns.compareAsc(dateToFormatTime, systemDate) === 0) {
+    return 'TODAY';
+  } else {
+    const dateFormat = 'DD/MM/YYYY'
+    const formattedDate = dateFns.format(dateToFormatTimeMillis, dateFormat)
+    return formattedDate
+  }  
 };
+
